668f4cabb1ed6ba1d835e101b6f9332dc7bec7b8
Roman Tsisyk <roman.tsisyk1@gmail.com>
2024-11-10 19:39:31 +0100

    save progress on refactor RepositoryProvider to ensure singleton pattern for RickAndMortyRepository

--- rickandmortyrepository/src/main/java/io/github/romantsisyk/rickandmortyrepositoryib/di/RepositoryProvider.kt
+++ rickandmortyrepository/src/main/java/io/github/romantsisyk/rickandmortyrepositoryib/di/RepositoryProvider.kt
@@ -1,18 +1,37 @@
-package io.github.romantsisyk.rickandmortyrepositoryib.di
-
-
-import io.github.romantsisyk.rickandmortydatabaselib.dao.CharacterDao
-import io.github.romantsisyk.rickandmortydatabaselib.dao.EpisodeDao
-import io.github.romantsisyk.rickandmortydatabaselib.dao.LocationDao
-import io.github.romantsisyk.rickandmortyrepositoryib.repository.RickAndMortyRepository
-import io.github.romantsisyk.rickandmortyrepositoryib.repository.RickAndMortyRepositoryImpl
-
-class RepositoryProvider {
-    fun provideRickAndMortyRepository(
-        characterDao: CharacterDao,
-        episodeDao: EpisodeDao,
-        locationDao: LocationDao
-    ): RickAndMortyRepository {
-        return RickAndMortyRepositoryImpl(characterDao, episodeDao, locationDao)
-    }
-}
+//package io.github.romantsisyk.rickandmortyrepositorylib.di
+//
+//
+//import io.github.romantsisyk.rickandmortyapilib.data.api.RickAndMortyService
+//import io.github.romantsisyk.rickandmortyapilib.domain.repository.RickAndMortyRepository
+//import io.github.romantsisyk.rickandmortydatabaselib.dao.CharacterDao
+//import io.github.romantsisyk.rickandmortydatabaselib.dao.EpisodeDao
+//import io.github.romantsisyk.rickandmortydatabaselib.dao.LocationDao
+//import io.github.romantsisyk.rickandmortyrepositoryib.repository.RickAndMortyRepositoryImpl
+//import io.github.romantsisyk.rickandmortyrepositoryib.util.ConnectivityChecker
+//
+//class RepositoryProvider {
+//
+//    private var repository: RickAndMortyRepository? = null
+//
+//    fun provideRepository(
+//        characterDao: CharacterDao,
+//        episodeDao: EpisodeDao,
+//        locationDao: LocationDao,
+//        service: RickAndMortyService,
+//        connectivityChecker: ConnectivityChecker
+//    ): RickAndMortyRepository {
+//        return repository ?: synchronized(this) {
+//            repository ?: RickAndMortyRepositoryImpl(
+//                characterDao = characterDao,
+//                episodeDao = episodeDao,
+//                locationDao = locationDao,
+//                service = service,
+//                connectivityChecker = connectivityChecker
+//            ).also { repository = it }
+//        }
+//    }
+//
+//    fun reset() {
+//        repository = null
+//    }
+//}
